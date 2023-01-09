2cd0342e7a17d163e34f2422a040d6f048987670
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-09 08:52:02 +0100

    Restore Hamburger Menu

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,7 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 import { StarWarsDataService } from '../star-wars-data.service';
 import { MatTable } from '@angular/material/table';
 import { Person } from '../interfaces';
@@ -12,19 +9,11 @@ import { Person } from '../interfaces';
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
   people: any = [];
   @ViewChild(MatTable) table?: MatTable<any>;
 
 
   constructor(
-    private breakpointObserver: BreakpointObserver,
     private dataService: StarWarsDataService
             ) {}
 
