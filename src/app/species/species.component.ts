925f75e08ab76359d5c7a7d7e1ea21305d624cc9
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-13 11:09:05 +0100

    Replace species list table with new data table component

--- src/app/species/species.component.ts
+++ src/app/species/species.component.ts
@@ -1,7 +1,5 @@
-import { Component, ViewChild, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Species } from '../interfaces';
-import { PageEvent } from '@angular/material/paginator';
-import { MatTable } from '@angular/material/table';
 import { StarWarsDataService } from '../star-wars-data.service';
 
 @Component({
@@ -12,40 +10,37 @@ import { StarWarsDataService } from '../star-wars-data.service';
 export class SpeciesComponent implements OnInit {
   loaded: boolean = false;
   species: Species[] = [];
-  totalSpeciesCount: number = 0;
-  displayedColumns: string[] = ['id', 'name', 'url'];
-  paginatorEnabled: boolean = true;
-  @ViewChild(MatTable) table?: MatTable<any>;
+  pagination = {length: 0, paginatorEnabled: true};
+  columns = {
+    'id' : {name: 'ID'},
+    'name' : {name: 'Name'},
+    'url' : {name: 'Details', url: ['/person/','id']},
+  }
+
 
   constructor(private dataService: StarWarsDataService){}
 
-  renderRows() {
-    this.table?.renderRows();
-  }
 
   getSpecies(page: number): void {
     this.dataService.getPage("species", page).subscribe(data => {
       this.species = data;
       this.species.sort((a: Species, b: Species) => a.id - b.id)
-      this.renderRows();
-      this.paginatorEnabled = true;
+      this.getSpeciesCount();
     });
   }
 
   getSpeciesCount() {
     this.dataService.getRecordCount("species").subscribe({
-      next: (count: number) => {this.totalSpeciesCount = count},
+      next: (count: number) => {this.pagination.length = count},
       complete: () => (this.loaded = true),
     });
   }
 
-  handlePageEvent(event: PageEvent) {
-    this.paginatorEnabled = false;
-    this.getSpecies(event.pageIndex + 1);
+  handlePageEvent(newPage: any) {
+    this.getSpecies(newPage);
   }
 
   ngOnInit(): void {
     this.getSpecies(1);
-    this.getSpeciesCount();
   }
 }
