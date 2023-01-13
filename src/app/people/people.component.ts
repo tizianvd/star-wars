dc6adc8884edf31e73a3ab23f3b8e4d3d6311474
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-13 11:02:20 +0100

    Replace people list table with new data table component

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,8 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { StarWarsDataService } from '../star-wars-data.service';
-import { MatTable } from '@angular/material/table';
 import { Person } from '../interfaces';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-people',
@@ -12,37 +10,32 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 export class PeopleComponent {
   people: any = [];
   loaded: boolean = false;
-  totalPeopleCount: number = 0;
-  paginatorEnabled: boolean = true;
-  @ViewChild(MatTable) table?: MatTable<any>;
+  pagination = {length: 0, paginatorEnabled: true};
+  columns = {
+    'id' : {name: 'ID'},
+    'name' : {name: 'Name'},
+    'url' : {name: 'Details', url: ['/person/','id']},
+  }
 
 
   constructor(
     private dataService: StarWarsDataService
             ) {}
 
-  renderRows() {
-    this.table?.renderRows();
-  }
-
   getPeople(page: number): void {
     this.dataService.getPage("people", page).subscribe(data => {
       this.people = data;
       this.people.sort((a: Person, b: Person) => a.id - b.id)
-      this.renderRows();
       this.loaded = true;
-      this.paginatorEnabled = true;
-      
     });
   }
 
   getTotalPeopleCount() {
-    this.dataService.getRecordCount("people").subscribe(count => this.totalPeopleCount = count);
+    this.dataService.getRecordCount("people").subscribe(count => this.pagination.length = count);
   }
 
-  handlePageEvent(event: PageEvent) {
-    this.paginatorEnabled = false;
-    this.getPeople(event.pageIndex + 1);
+  handlePageEvent(event: any) {
+    this.getPeople(event);
   }
 
 
@@ -50,6 +43,5 @@ export class PeopleComponent {
     this.getPeople(1);
     this.getTotalPeopleCount();
   }
-  displayedColumns: string[] = ['id', 'name', 'url'];
   
 }
