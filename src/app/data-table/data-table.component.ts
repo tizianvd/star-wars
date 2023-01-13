004ca8fa09c03bb5c7fe9f59e13cfcf71c3108ae
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-13 10:58:01 +0100

    Replace planets table with ne data table component

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Output, Input, EventEmitter, ViewChild, OnChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { PageEvent } from '@angular/material/paginator';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent {
+export class DataTableComponent implements OnChanges {
   @Input() tableData: any;
   @Input() columns: any;
+  @Input() pagination: any;
+
+  @Output() pageEvent = new EventEmitter;
+
+  @ViewChild(MatTable) table?: MatTable<any>;
 
   dataSource: any;
   objectKeys = Object.keys;
 
-  ngOnInit() {
+  ngOnChanges() {
     this.dataSource = new MatTableDataSource(this.tableData);
+    this.pagination.paginatorEnabled = true;
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.pagination.paginatorEnabled = false;
+    this.pageEvent.emit(event.pageIndex + 1);
   }
 }
--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { StarWarsDataService } from '../star-wars-data.service';
 import { Planet } from '../interfaces';
-import { MatTable } from '@angular/material/table';
-import { PageEvent } from '@angular/material/paginator';
 
 
 @Component({
@@ -13,35 +11,26 @@ import { PageEvent } from '@angular/material/paginator';
 export class PlanetsComponent implements OnInit {
   planets: any = [];
   loaded: boolean = false;
-  totalPlanetCount: number = 0;
-  paginatorEnabled: boolean = true;
-  @ViewChild(MatTable) table?: MatTable<any>;
+  pagination = {length: 0, paginatorEnabled: true};
 
   constructor(
     private dataService: StarWarsDataService
             ) {}
 
-  renderRows(){
-    this.table?.renderRows();
-  }
-
   getPlanets(page: number): void {
     this.dataService.getPage("planets", page).subscribe(
       data => {this.planets = data
       this.planets.sort((a: Planet, b: Planet) => a.id - b.id)
-      this.renderRows();
       this.loaded = true;
-      this.paginatorEnabled = true;
     });
   }
 
   getTotalPlanetCount() {
-    this.dataService.getRecordCount("planets").subscribe(count => this.totalPlanetCount = count);
+    this.dataService.getRecordCount("planets").subscribe(count => this.pagination.length = count);
   }
 
-  handlePageEvent(event: PageEvent) {
-    this.paginatorEnabled = false;
-    this.getPlanets(event.pageIndex + 1);
+  handlePageEvent(event: any) {
+    this.getPlanets(event);
   }
 
   ngOnInit() {
@@ -49,5 +38,9 @@ export class PlanetsComponent implements OnInit {
     this.getTotalPlanetCount();
   }
 
-  displayedColumns: string[] = ['id', 'name', 'url'];
+  columns = {
+              'id' : {name: 'ID'},
+              'name' : {name: 'Name'},
+              'url' : {name: 'Details', url: ['/planet-details/','id']},
+}
 }
