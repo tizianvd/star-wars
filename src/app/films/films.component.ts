e3f8bdc45178f83864f157c3016b73a8d01e2a4f
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-13 11:05:54 +0100

    Replace films table with new data table component

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Film } from '../interfaces';
 import { StarWarsDataService } from '../star-wars-data.service';
-import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-films',
@@ -10,23 +9,19 @@ import { MatTable } from '@angular/material/table';
 })
 export class FilmsComponent implements OnInit {
   films: any[] = [];
-  displayedColumns: string[] = ['id', 'title', 'url'];
   loaded: boolean = false;
- 
-  @ViewChild(MatTable) table?: MatTable<any>;
+  columns = {
+    'id' : {name: 'ID'},
+    'title' : {name: 'Title'},
+    'url' : {name: 'Details', url: ['/film-details/','id']},
+  }
 
   constructor(private dataService: StarWarsDataService){}
-
-  renderRows(){
-    this.table?.renderRows();
-  }
 
   getFilms(): void {
     this.dataService.getPage("films", 1).subscribe({
       next: data => {this.films = data
       this.films.sort((a: Film, b: Film) => a.id - b.id)
-
-      this.renderRows();
       },
       complete: () => {this.loaded = true}
     });
