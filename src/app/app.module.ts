ae72a320a8eab8ea91efcdf1b14ab920f6c49ff1
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-23 16:22:57 +0100

    Work on search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -61,6 +62,7 @@ import { StarshipDetailsComponent } from './starship-details/starship-details.co
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
     LayoutModule,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarWarsDataService } from '../star-wars-data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent {
-  constructor() {}
+export class DashboardComponent implements OnInit {
+  constructor(private dataService: StarWarsDataService) {}
+  searchPhrase: string = "";
+
+  search() : void {
+    this.dataService.search("people", "luke")
+    .subscribe(
+      film => {
+        console.log(film)
+
+      }
+    );
+  }
+
+  onType(event: Event) {
+    console.log(this.searchPhrase);
+  }
+
+  ngOnInit(): void {
+    //this.search();
+  }
 }
