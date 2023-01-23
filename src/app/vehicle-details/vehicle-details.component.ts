721248119691d246ff6fcd8d879ecae29f9d0518
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-23 09:30:41 +0100

    Add pilots to vehicle details

--- src/app/vehicle-details/vehicle-details.component.ts
+++ src/app/vehicle-details/vehicle-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Vehicle, Film } from '../interfaces';
+import { Vehicle, Film, Person } from '../interfaces';
 import { StarWarsDataService } from '../star-wars-data.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -11,7 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 export class VehicleDetailsComponent implements OnInit {
   vehicle!: Vehicle;
   loaded: boolean = false;
+  pilots: Person[] = [];
   films: Film[] = [];
+  pilotsTableColumns = {'id' : {name: 'ID'}, 
+  'name' : {name: 'Name'}, 
+  'url' : {name: 'Details', url:['/person/', 'id']}
+  };
   filmsTableColumns = {'id' : {name: 'ID'}, 
   'title' : {name: 'Title'}, 
   'url' : {name: 'Details', url:['/film-details/', 'id']}
@@ -28,9 +33,28 @@ export class VehicleDetailsComponent implements OnInit {
     .subscribe(
       vehicle => {
         this.vehicle = vehicle
+        this.getPilots();
         this.getFilms();
       }
     );
+  }
+
+  getPilots(): void {
+    this.dataService.getAllRecords("people").subscribe({
+      next: data => {
+      data.forEach(element => {
+        if (this.vehicle.pilots.includes(element.url)){
+          this.pilots.push(element);
+          this.pilots.sort((a: Person, b: Person) => a.id - b.id)
+
+        }
+      });
+      
+      }, 
+      complete: () => {
+        this.loaded = true;
+        }
+      });
   }
 
   getFilms(): void {
