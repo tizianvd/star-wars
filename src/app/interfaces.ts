5baca3f0806d703ca6b257ba21e69960dcfb737a
TVanDahl@itemkg.de <TVanDahl@itemkg.de>
2023-01-23 09:46:57 +0100

    Add starships list

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -75,3 +75,22 @@ export interface Vehicle {
     films: Film[];
     pilots: Person[];
 }
+
+export interface Starship {
+    id: number;
+    name: string;
+    model: string;
+    starship_class: string;
+    manufacturer: string;
+    cost_in_credits: string;
+    length: string;
+    crew: string;
+    passengers: string;
+    max_atmosphering_speed: string;
+    hyperdrive_rating: string;
+    MGLT: string;
+    cargo_capacity: string;
+    consumables: string;
+    films: Film[];
+    pilots: Person[]
+}
--- src/app/starships/starships.component.ts
+++ src/app/starships/starships.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Starship } from '../interfaces';
+import { StarWarsDataService } from '../star-wars-data.service';
 
 @Component({
   selector: 'app-starships',
   templateUrl: './starships.component.html',
   styleUrls: ['./starships.component.css']
 })
-export class StarshipsComponent {
+export class StarshipsComponent implements OnInit {
+  loaded: boolean = false;
+  starships: Starship[] = [];
+  pagination = {length: 0, paginatorEnabled: true};
+  columns = {
+    'id' : {name: 'ID'},
+    'name' : {name: 'Name'},
+    'url' : {name: 'Details', url: ['/starship-details/','id']},
+  }
 
+  constructor(private dataService: StarWarsDataService){}
+
+  getStarships(page: number): void {
+    this.dataService.getPage("starships", page).subscribe(data => {
+      this.starships = data;
+      this.starships.sort((a: Starship, b: Starship) => a.id - b.id)
+      this.getStarshipCount();
+    });
+  }
+
+  getStarshipCount() {
+    this.dataService.getRecordCount("starships").subscribe({
+      next: (count: number) => {this.pagination.length = count},
+      complete: () => (this.loaded = true),
+    });
+  }
+
+  
+  handlePageEvent(newPage: any) {
+    this.getStarships(newPage);
+  }
+
+  ngOnInit(): void {
+    this.getStarships(1);
+  }
 }
